c915710d6d64fd8136c00c3217a5e76a215ca2a6
Emma :) <99496596+kodukass@users.noreply.github.com>
2024-02-29 12:17:27 +0200

    kodutöö

---
+++ kodutöö3.ts
@@ -0,0 +1,35 @@
+//on vibud ja jubinad (vibude jaoks)
+class Vibu 
+{
+    name: string;
+    jubin: any;
+
+    constructor(name: string)
+    {
+        this.name=name;
+    }
+}
+
+class Jubin 
+{
+    name: string;
+
+    constructor(name: string)
+    {
+        this.name=name;
+    }
+
+    addJubin(vibu: Vibu): void {
+        if (vibu.jubin == null) {
+            this.name.push(name);
+            vibu.jubin = this;
+        }
+    }
+}
+//kui vibul on jubinad siis ütleb et on sportvibu, kui ei siis on vaistuvibu
+let vibu1 = new Vibu("Vibu1");
+let vibu2 = new Vibu("Vibu2");
+
+let stabikas:Jubin = new Jubin("Stabilisaator");
+let joonlaud:Jubin = new Jubin("Sihik");
+
